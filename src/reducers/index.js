a15afc510092b2461bb702baa743317ee6d35450
DEEPAK KUMAR <deepakkumar7228488@gmail.com>
2021-06-11 11:33:53 +0530

    Added the small react redux app

---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+import changeTheNumber from './upDown';
+
+import { combineReducers } from 'redux'; //import from redux
+
+//object
+const rootReducer = combineReducers({
+  changeTheNumber,
+});
+
+export default rootReducer;
